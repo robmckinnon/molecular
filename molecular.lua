8973564d2e81d53dad658df7b3b8ac4701dd372a
robmckinnon <rob@movingflow>
2021-01-17 17:09:00 +0000

    Use softcut to record loop; only play new notes

--- molecular.lua
+++ molecular.lua
@@ -34,18 +34,19 @@ local western=require 'musicutil'
 
 engine.name = 'PolyPerc'
 local bars = 4
-local step_div = 1
+local step_div = 2
 local beats_per_bar = 4
 local step_size
 local steps_per_bars
 local steps_count
 local next_on_step
 local step = 0
-local duration1 = 7
-local duration2 = 15.5
+local duration1 = 9
+local duration2 = 14.5
 local duration = 9
 local onsteps = {}
 local offsteps = {}
+local newsteps = {}
 local degree = 1
 local scale_degrees = scale.length
 local octave = 5
@@ -55,6 +56,17 @@ options.OUTPUT = {"audio", "midi", "audio + midi", "crow out 1+2", "crow ii JF"}
 local midi_out_device
 local midi_out_channel
 local active_notes = {}
+
+-- softcut vars
+local rate = 1.0
+local rec = 1.0
+local pre = 0.0
+
+local off = 0
+local on = 1
+local end_of_loop = 61
+local time_ref = 0
+local initial_loop = true
 
 function init()
   print("init")
@@ -92,10 +104,11 @@ function init()
   params:set_action("midi_out_device", function(x) midi_out_device = midi.connect(x) end)
   params:set_action("midi_out_channel", function(x) all_notes_off(); midi_out_channel = x end)
 
+  initial_loop = true
+  step = 0
   next_on_step = 1
   duration = duration1
   calc_steps()
-
   loop_id = clock.run(step_loop)
 end
 
@@ -103,6 +116,51 @@ function key(n,z)
   if n == 3 and z == 1 then
   elseif n == 2 and z == 1 then
     clock.cancel(loop_id)
+  end
+end
+
+function init_recording()
+  print("init_recording")
+  softcut.buffer_clear()
+  time_ref = util.time()
+  -- audio.level_adc_cut(0.75)
+
+  for i = 1,2 do
+    -- ch 1 -> voice 1   ch 2 -> voice 2
+    softcut.level_input_cut(i,i,1)
+    -- voice 1 -> buffer 1  voice 2 -> buffer 2
+    softcut.buffer(i,i)
+    -- softcut.pan(i, pan[i])
+
+    softcut.play(i,on)
+    softcut.rec(i,on)
+    softcut.enable(i,on)
+    softcut.rec_offset(i,-0.06)
+
+    -- preserve existing recording amplitude
+    softcut.pre_level(i,1)
+    -- record at full amplitude
+    softcut.rec_level(i, 1)
+
+    -- output level
+    softcut.level(i,0.75)
+    -- set crossfaded looping mode
+    softcut.loop(i,1)
+    -- set loop start at 1 second
+    softcut.loop_start(i,1)
+    -- set loop end at end_of_loop+1 seconds
+    softcut.loop_end(i,end_of_loop + 1)
+    -- set play position
+    softcut.position(i,1)
+    -- set playback rate to normal
+    softcut.rate(i,1.0)
+    -- set fade time position to 0 seconds
+    softcut.fade_time(i,0)
+
+    -- set slew time to 0.5 seconds?
+    softcut.level_slew_time(i,0.5)
+    -- set rate slew time to 0.05 seconds?
+    softcut.rate_slew_time(i,0.05)
   end
 end
 
@@ -148,18 +206,30 @@ function note_on(deg_oct)
   local deg = deg_oct[1]
   local oct = deg_oct[2]
   local freq = pitches:octdegfreq(oct, deg)
-  local note_num = western.freq_to_note_num(freq)
-  play_note_on(freq, note_num)
-  print(step.." on: "..deg.." "..oct.." note_num: "..note_num)
+  if freq then
+    local note_num = western.freq_to_note_num(freq)
+    play_note_on(freq, note_num)
+    print(step.." on: "..deg.." "..oct.." note_num: "..note_num)
+    return true
+  else
+    print(step.." on: out of notes: "..deg.." "..oct)
+    return false
+  end
 end
 
 function note_off(deg_oct)
-  local deg = deg_oct[1]
-  local oct = deg_oct[2]
-  local freq = pitches:octdegfreq(oct, deg)
-  local note_num = western.freq_to_note_num(freq)
-  midi_out_device:note_off(note_num, 0, midi_out_channel)
-  print(step.." off: "..deg.." "..oct.." "..western.freq_to_note_num(freq))
+  if midi_output() then
+    local deg = deg_oct[1]
+    local oct = deg_oct[2]
+    local freq = pitches:octdegfreq(oct, deg)
+    if freq then
+      local note_num = western.freq_to_note_num(freq)
+      midi_out_device:note_off(note_num, 0, midi_out_channel)
+      print(step.." off: "..deg.." "..oct.." "..western.freq_to_note_num(freq))
+    else
+      print(step.." off: out of notes: "..deg.." "..oct)
+    end
+  end
 end
 
 function all_notes_off()
@@ -186,6 +256,7 @@ end
 
 function add_onstep(step)
   onsteps[step][#onsteps[step] + 1] = {degree, octave}
+  newsteps[step] = {degree, octave}
 end
 
 function add_offstep(step)
@@ -199,9 +270,42 @@ function add_offstep(step)
   print("next_on_step: "..next_on_step)
 end
 
+function set_end_of_loop()
+  end_of_loop = util.time() - time_ref + 1
+  print("end_of_loop: "..end_of_loop)
+  softcut.loop_end(1,end_of_loop)
+  softcut.loop_end(2,end_of_loop)
+end
+
+function stop_recording()
+  softcut.rec_level(1,off)
+  softcut.rec_level(2,off)
+end
+
+function reset_recording_loop()
+  softcut.rec_level(1,off)
+  softcut.rec_level(2,off)
+  softcut.position(1,1)
+  softcut.position(2,1)
+  softcut.rec_level(1,on)
+  softcut.rec_level(2,on)
+end
+
 function increment_step()
+  if initial_loop and step == 0 then
+    init_recording()
+  end
+
   step = step + 1
-  if (step > steps_count) then step = 1 end
+  if (step > steps_count) then
+    -- stop_recording()
+    if initial_loop then
+      set_end_of_loop()
+      initial_loop = false
+    end
+    reset_recording_loop()
+    step = 1
+  end
 end
 
 function increment_scale_degree()
@@ -221,15 +325,22 @@ function notes_off(step)
 end
 
 function notes_on(step)
+  local more_notes = true
   if onsteps[step] then
-    for i, deg_oct in pairs(onsteps[step]) do
-      note_on(deg_oct)
+    if midi_output() and newsteps[step] then
+      more_notes = more_notes and note_on(newsteps[step])
+    else
+      for i, deg_oct in pairs(onsteps[step]) do
+        more_notes = more_notes and note_on(deg_oct)
+      end
     end
   end
+  return more_notes
 end
 
 function step_loop()
-  while true do
+  local more_notes = true
+  while more_notes or (step < steps_count) do
     clock.sync(step_size)
     increment_step()
     print("step: "..step)
@@ -246,8 +357,10 @@ function step_loop()
       increment_scale_degree()
     end
 
-    notes_on(step)
+    more_notes = notes_on(step)
   end
+  stop_recording()
+  print("step loop complete")
 end
 
 function calc_steps()
